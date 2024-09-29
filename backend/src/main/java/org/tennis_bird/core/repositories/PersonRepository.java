412a5be86cbb348907fd348bf39d35a62c2996dd
katierevinska <katerevinska05@gmail.com>
2024-09-29 22:07:09 +0300

    add test

--- backend/src/main/java/org/tennis_bird/core/repositories/PersonRepository.java
+++ backend/src/main/java/org/tennis_bird/core/repositories/PersonRepository.java
@@ -1,7 +1,21 @@
 package org.tennis_bird.core.repositories;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.tennis_bird.core.entities.PersonEntity;
+import org.tennis_bird.core.services.PersonService;
 
-public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
+import javax.transaction.Transactional;
+import java.util.UUID;
+
+public interface PersonRepository extends JpaRepository<PersonEntity, UUID> {
+
+    @Transactional
+    default PersonEntity updateOrInsert(PersonEntity person) {
+        return save(person);
+    }
 }
